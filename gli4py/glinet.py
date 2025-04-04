cbf2a67f14b77f84d3b792d033fbf50c57b2356d
HarvsG <11440490+HarvsG@users.noreply.github.com>
2025-04-04 15:40:20 +0100

    Revert "Add reboot command"

--- gli4py/glinet.py
+++ gli4py/glinet.py
@@ -144,9 +144,6 @@ class GLinet(Consumer):
     async def router_mac(self) -> dict:
         return await self._request(self.gen_sid_payload('call', ['macclone', 'get_mac'], self.sid))
 
-    async def router_reboot(self, delay: int = 0) -> dict:
-        return await self._request(self.gen_sid_payload('call', ['system', 'reboot',  {'delay': delay}], self.sid))
-
     async def ping(self, address) -> bool:
         """
         returns the stdout of the ping command if successful or "[]" if not successful
