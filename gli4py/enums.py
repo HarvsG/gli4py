e45ad2bf7e7a6d79587f7eaf7dfa51a246b96d8b
HarvsG <11440490+HarvsG@users.noreply.github.com>
2025-07-22 14:25:54 +0100

    Enable linting compliance (#14)
    * Create pylint.yml
    
    * linting
    
    * fix errors

--- gli4py/enums.py
+++ gli4py/enums.py
@@ -1,11 +1,13 @@
 """This module defines enums for various states and types used in the GL-inet API client."""
+
 from enum import Enum
+
 
 class TailscaleConnection(Enum):
     """Enum representing the connection states of Tailscale."""
-    
-    Disconnected = 0
-    LoginRequired = 1
-    AuthorizationRequired = 2
-    Connected = 3
-    Connecting = 4
+
+    DISCONNECTED = 0
+    LOGIN_REQUIRED = 1
+    AUTHORIZATION_REQUIRED = 2
+    CONNECTED = 3
+    CONNECTING = 4
