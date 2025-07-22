d724e1d68f09669ca7611d126bf55072609592e9
HarvsG <11440490+HarvsG@users.noreply.github.com>
2025-07-22 13:26:43 +0100

    adds MORE docstrings

--- gli4py/enums.py
+++ gli4py/enums.py
@@ -1,6 +1,9 @@
+"""This module defines enums for various states and types used in the GL-inet API client."""
 from enum import Enum
 
 class TailscaleConnection(Enum):
+    """Enum representing the connection states of Tailscale."""
+    
     Disconnected = 0
     LoginRequired = 1
     AuthorizationRequired = 2
